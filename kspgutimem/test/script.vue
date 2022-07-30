872cca8211b30796a1a4f7c6ab3ad277fba52a5c
my-cla4ik-f0rmy <89280222+my-cla4ik-f0rmy@users.noreply.github.com>
2022-07-30 19:08:09 +0400

    Add files via upload

---
+++ kspgutimem/test/script.vue
@@ -0,0 +1,29 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+
+
+const Test ={
+  data() {
+    return {
+      picked: 'hello'
+      
+    }
+  }
+}
+
+Vue.createApp(Test).mount('#v-model-radiobutton')
+
+
+
+
+
+
+
+  
